047159744ab45f3dd0485cf62893860a80ce9ea2
Abner <abner.a.filho@gmail.com>
2026-08-18 20:21:51 -0300

    feat: primeira aula de Python

---
+++ 03_variaveis.py
@@ -0,0 +1,18 @@
+# %%
+nome = "Abner"
+print(nome)
+
+nome = "Garrafa"
+print(nome)
+
+# %%
+resultado = 10 - 5
+print(resultado)
+
+resultado = 10 * 2
+print(resultado)
+
+resultado = resultado + 10
+print(resultado)
+
+# %%
