a88f1d509cd0fc272c3574b4bbfc8199b6f20152
Abner <abner.a.filho@gmail.com>
2026-08-18 22:50:52 -0300

    feat: if else dia 02

---
+++ dia_02/04_logica.py
@@ -0,0 +1,7 @@
+# %%
+print("Comparações lógicas")
+print("1 > 2 =", 1 > 2)
+print("2 > 2 =", 2 > 2)
+print("2 >= 2 =", 2 >= 2)
+print("10 > 5 =", 10 > 5)
+print("Abner == Abner =", "Abner" == "Abner")
