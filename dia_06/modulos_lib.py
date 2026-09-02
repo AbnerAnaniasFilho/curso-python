f4f0763e3abcfd2a6d31580e9125bb7327ca914c
Abner <abner.a.filho@gmail.com>
2026-09-01 22:37:39 -0300

    feat: functions modules libraries dia 06

---
+++ dia_06/modulos_lib.py
@@ -0,0 +1,21 @@
+# %%
+import math
+from math import pi, e
+
+# %%
+math.sqrt(9)
+
+# %%
+math.pow(2, 4)
+
+# %%
+math.e
+
+# %%
+math.pi
+
+# %%
+pi
+
+# %%
+e
---
+++ dia_06/random.py
@@ -0,0 +1,10 @@
+# %%
+import random
+
+# %%
+random.randint(1, 10)
+
+# %%
+x = ["Abner", "Maria", "Jose", "Ana", "João"]
+
+random.choice(x)
